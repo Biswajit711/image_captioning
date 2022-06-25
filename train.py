25a2f05cf152e2a3ac987bc97cebe29b34896f1d
Biswajit711 <52106583+Biswajit711@users.noreply.github.com>
2022-06-25 21:26:10 +0530

    add cmment t0 test

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from model import EncoderCNN, DecoderRNN
 from torch.nn.utils.rnn import pack_padded_sequence
 from torchvision import transforms
 
-
+# fr test
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
